4cf87bcdfdf43436fd50ea6151d0c1e18ebb6091
Matt W <matwilson@paloaltonetworks.com>
2025-02-12 17:31:16 -0500

    updated bucket name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "your-terraform-state-bucket" # Change this to your S3 bucket name
+    bucket         = "tf-state-bucket-0123456789" # Change this to your S3 bucket name
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks" # Optional: If using DynamoDB for state locking
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "documents" {
   bucket = var.s3_bucket_name
 }
+
+resource "aws_s3_bucket" "documents_log_bucket" {
+  bucket = "documents-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "documents" {
+  bucket = aws_s3_bucket.documents.id
+
+  target_bucket = aws_s3_bucket.documents_log_bucket.id
+  target_prefix = "log/"
+}
+
+
 resource "aws_s3_bucket_public_access_block" "block_public" {
   bucket                  = aws_s3_bucket.documents.id
   block_public_acls       = true
